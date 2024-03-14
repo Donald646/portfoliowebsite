025d939ed2329233554bfa33efa04c049659231d
Donald646 <donaldchu901@gmail.com>
2024-03-13 22:55:21 -0500

    responsive tech stack

--- src/app/_components/singleTech.js
+++ src/app/_components/singleTech.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function SingleTech({ name, src }) {
   return (
     <div className="flex flex-col mt-3 p-3 border-2 items-center rounded-xl">
-      <img className="w-auto h-16" src={src} />
+      <img className="w-auto h-16 sm:h-8" src={src} />
       <p className="text-base text-center mt-1">{name}</p>
     </div>
   );
