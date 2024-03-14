34a698966f83b79c6fa8e514049ff8f065278175
Donald646 <donaldchu901@gmail.com>
2024-03-14 14:45:06 -0500

    changed contact email

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="text-white">
       <h1 className="text-5xl text-center">Contact Me</h1>
-      <p className="text-2xl text-center mt-10">donaldchu901@gmail.com</p>
+      <p className="text-2xl text-center mt-10">donaldvchu@gmail.com</p>
     </div>
   );
 }
